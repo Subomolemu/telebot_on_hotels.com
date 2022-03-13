ad8a02b3e1d009995d961b2483e070684d70579e
Sergey Sinkov <subomolemu@gmail.com>
2022-03-13 20:48:54 +0300

    измененя по видео Состояние пользователя внутри сценария

---
+++ handlers/custom_handlers/survey.py
@@ -0,0 +1,71 @@
+from loader import bot
+from states.contact_information import UserInfoState
+from telebot.types import Message
+from keyboards.reply.contact import request_contact
+
+
+@bot.message_handler(commands=['survey'])
+def survey(message: Message):
+    bot.set_state(message.from_user.id, UserInfoState.name, message.chat.id)
+    bot.send_message(message.from_user.id, f'Привет, {message.from_user.username}, введи свое имя')
+    
+    
+@bot.message_handler(state=UserInfoState.name)
+def get_name(message: Message) -> None:
+    if message.text.isalpha():
+        bot.send_message(message.from_user.id, 'Спасибо, записал, теперь введи свой возраст')
+        bot.set_state(message.from_user.id, UserInfoState.age, message.chat.id)
+        
+        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+            data['name'] = message.text
+    else:
+        bot.send_message(message.from_user.id, 'Имя должно состоять из букв')
+
+
+@bot.message_handler(state=UserInfoState.age)
+def get_age(message: Message) -> None:
+    if message.text.isdigit():
+        bot.send_message(message.from_user.id, 'Спасибо, записал, теперь введи свою страну')
+        bot.set_state(message.from_user.id, UserInfoState.country, message.chat.id)
+        
+        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+            data['age'] = message.text
+    else:
+        bot.send_message(message.from_user.id, 'Возраст должен состоять из цифр')
+
+
+@bot.message_handler(state=UserInfoState.country)
+def get_city(message: Message) -> None:
+    bot.send_message(message.from_user.id, 'Спасибо, записал, теперь введи свой город')
+    bot.set_state(message.from_user.id, UserInfoState.city, message.chat.id)
+    
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['country'] = message.text
+
+
+@bot.message_handler(states=UserInfoState.city)
+def get_city(message: Message) -> None:
+    bot.send_message(message.from_user.id, 'Спасибо, записал. Отправь свой номер телефона, '
+                                           'нажав на кнопку', reply_markup=request_contact())
+    bot.set_state(message.from_user.id, UserInfoState.phone_number, message.chat.id)
+    
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['city'] = message.text
+        
+
+@bot.message_handler(content_types=['text', 'contack'], state=UserInfoState.phone_number)
+def get_contact(message: Message) -> None:
+    if message.content_type == 'contact':
+        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+            data['phone_number'] = message.contact.phone_number
+            
+            text = f'Спасибо за предоставленную информацию, ваши данные:\n' \
+                   f'    Имя - {data["name"]}\n' \
+                   f'    Возраст - {data["age"]}\n' \
+                   f'    Страна - {data["country"]}\n' \
+                   f'    Город - {data["city"]}\n' \
+                   f'    Номер телефона - {data["phone_number"]}'
+            bot.send_message(message.from_user.id, text)
+            
+    else:
+        bot.send_message(message.from_user.id, 'Отправьте номер телефона, нажав на кнопку')
