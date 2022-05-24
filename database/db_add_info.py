f3cf7cf4ea19bf3de03c4b45fc2496e36f29e5ed
Segey Sergeevich <subomolemu@gmail.com>
2022-05-24 17:05:21 +0300

    Небольшие изменения. История воводит информацию за текущие сутки небольшие изменения в плане аннотаций типов и документации

--- database/db_add_info.py
+++ database/db_add_info.py
@@ -1,7 +1,16 @@
 from . import main_db
+from typing import List
 
 
-def add(user_id, command_date, command, hotels):
+def add(user_id: str, command_date: str, command: str, hotels: List) -> None:
+    """
+    Функция предназначена для записи следующей информации об истории поиска пользователем: дата ввода команды,
+    сама команда, списко найденных отелей.
+    :param user_id: ид пользователя
+    :param command_date: время введенной команды от пользователя
+    :param command: команда, введенная пользователем
+    :param hotels: список полученных отелей
+    """
     hotels = '\n- ' + '\n- '.join(hotels)
     with main_db.db:
         main_db.Info.create(user_id=user_id, command_time=command_date, command=command, hotels=hotels)
--- database/db_out_info.py
+++ database/db_out_info.py
@@ -1,8 +1,17 @@
 from . import main_db
+from datetime import date, datetime
 
 
-def out(user_id):
+def out(user_id: int) -> str:
+    """
+    Фукция предназначена для создания генератора вывода истории поиска пользователю. История выдается только за
+    текущую дату. Вызывается при команде /history от пользователя.
+    :param user_id: ид пользователя
+    :return: выводт строку истории поиска, содержащую информацию о дате ввода команды, самой команде, списком найденных
+    по запросу отелей.
+    """
     with main_db.db:
         for i in main_db.Info.select().order_by(main_db.Info.user_id):
-            if i.user_id == str(user_id):
+            if i.user_id == str(user_id) and date.today() == \
+                    datetime.strptime(i.command_time, "%Y-%m-%d %H:%M:%S").date():
                 yield f'{i.command_time}, "{i.command}", \nCписок найденных отелей: {i.hotels}'
